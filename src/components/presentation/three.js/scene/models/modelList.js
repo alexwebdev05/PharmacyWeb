40779bb205ce207348e3488a9c86776b03297d48
sniffsec <sniffsec@proton.me>
2024-07-31 13:41:58 +0200

    models fix

--- src/components/presentation/three.js/scene/models/modelList.js
+++ src/components/presentation/three.js/scene/models/modelList.js
@@ -1,5 +1,5 @@
 export const models = [
-    'src/components/presentation/three.js/assets/models/alcohol.glb',
-    'src/components/presentation/three.js/assets/models/pills.glb',
-    'src/components/presentation/three.js/assets/models/antiSeptic.glb'
+    '/models/alcohol.glb',
+    '/models/pills.glb',
+    '/models/antiSeptic.glb'
   ]
